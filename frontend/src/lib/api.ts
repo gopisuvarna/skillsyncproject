3b3f29deffd221591be0f1fcd13defa1bd2129a7
gopisuvarna <bailapudigopisuvarna@gmail.com>
2026-04-01 10:31:54 +0530

    inital commit

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const api: AxiosInstance = axios.create({
   baseURL,
